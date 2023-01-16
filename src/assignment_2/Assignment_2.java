6d9c9de0485d07b62bb74d31091f5022f5e5ec19
jdykst02 <jdykstra04@gmail.com>
2023-01-16 11:43:20 -0500

    testing

---
+++ src/assignment_2/Assignment_2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignment_2;
+
+/**
+ *
+ * @author Reanna
+ */
+public class Assignment_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 5, y = 7;
+        if ( y == 8 ){
+            if ( x == 5 ){
+                System.out.println( "@@@@@" );
+            }
+        }else{
+            System.out.println( "#####" );            
+            System.out.println( "$$$$$" );
+            System.out.println( "&&&&&" );
+        }
+        
+        
+    }
+    
+}
